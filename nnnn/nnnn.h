62ee735b8a07983bbe093c3605bfa4290c052537
Moses Ayankoya <ayankoya@systemspecs.com.ng>
2019-06-21 12:36:51 +0100

    ... initial

---
+++ nnnn/nnnn.h
@@ -0,0 +1,19 @@
+//
+//  nnnn.h
+//  nnnn
+//
+//  Created by Moses on 21/06/2019.
+//  Copyright © 2019 IOpenSourced. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nnnn.
+FOUNDATION_EXPORT double nnnnVersionNumber;
+
+//! Project version string for nnnn.
+FOUNDATION_EXPORT const unsigned char nnnnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nnnn/PublicHeader.h>
+
+
